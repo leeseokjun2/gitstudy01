f9cdf4a3d6b08431af724103c0efd508a418ae15
leeseokjun2 <tjrwns000617@naver.com>
2024-05-27 13:25:54 +0900

    branch commit

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.end('<h1>Hello Worid!</h1>');
 });
 
+app.get('/home', (req, res) => {
+    res.end('<h1>Hello Home!!</h1>');
+});
+
 const server = http.createServer(app);
 server.listen(3000, () => {
     console.log('Run on server http://localhost:3000');
