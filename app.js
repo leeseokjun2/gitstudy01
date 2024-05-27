582a7bb7dc1706ea47cbdd249fdc7653ed5cad88
leeseokjun2 <tjrwns000617@naver.com>
2024-05-27 13:31:51 +0900

    add profile

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/home', (req, res) => {
     res.end('<h1>Hello Home!!</h1>');
 });
 
+app.get('/profile', (req, res) => {
+    res.end('<h1>Hello profile!!</h1>');
+});
+
 const server = http.createServer(app);
 server.listen(3000, () => {
     console.log('Run on server http://localhost:3000');
