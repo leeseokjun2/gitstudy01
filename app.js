486261d3407b9656df8a160f0c71ecd2918ea8c9
leeseokjun2 <tjrwns000617@naver.com>
2024-05-27 14:14:18 +0900

    first

--- app.js
+++ app.js
@@ -6,13 +6,6 @@ app.get('/', (req, res) => {
     res.end('<h1>Hello Worid!</h1>');
 });
 
-app.get('/home', (req, res) => {
-    res.end('<h1>Hello Home!!</h1>');
-});
-
-app.get('/profile', (req, res) => {
-    res.end('<h1>Hello profile!!</h1>');
-});
 
 const server = http.createServer(app);
 server.listen(3000, () => {
