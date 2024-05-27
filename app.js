af34142e10defa6742505626d01a7cf10b8dca43
leeseokjun2 <131133501+leeseokjun2@users.noreply.github.com>
2024-05-27 15:21:13 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.end('<h1>반갑다</h1>');
+    res.end('<h1>Hello Worid!</h1>');
 });
+
+app.get('/home', (req, res) => {
+    res.end('<h1>Hello Home!!</h1>');
+});
+
+app.get('/profile', (req, res) => {
+    res.end('<h1>Hello profile!!</h1>');
+});
+
+app.get('/shop', (req, res) => {
+    res.end('<h1>Hello shopping mall!!</h1>');
+});
+
 
 
 const server = http.createServer(app);
