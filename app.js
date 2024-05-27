b3ea79f324bd681a253dd425d0579f5821f1b72a
leeseokjun2 <tjrwns000617@naver.com>
2024-05-27 14:12:46 +0900

    step1:

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('/profile', (req, res) => {
     res.end('<h1>Hello profile!!</h1>');
 });
 
+app.get('/shop', (req, res) => {
+    res.end('<h1>Hello shopping mall!!</h1>');
+});
+
 const server = http.createServer(app);
 server.listen(3000, () => {
     console.log('Run on server http://localhost:3000');
